65e97974ea077c94c9e4f61bcbee0231fc6353a1
Ketzenkrad <ketzenkrad@gmail.com>
2019-06-02 21:59:03 +0300

    Сформирована веб-страница

--- node_test/index.js
+++ node_test/index.js
@@ -1,12 +1,18 @@
-//Подключение пакета в скрипт
+//Подключение пакета Express в скрипт
 const express = require('express');
 //Переменная приложения
 const application = express();
+//Вызовем у объекта метод ejs, который поможет нам шаблонизировать HTML
+application.set('view engine', 'ejs');
+//Обозначим express статическую папку
+application.use(express.static('public'));
+//Ответим пользователю через коллбэк
+application.get('/', (req, res) => {
+    //При запросе на корневой элемент рендерим индексный файл
+    res.render('index');
+});
 //Слушаем приложение(порт и коллбэк)
 application.listen(3000, () => {
-    console.log('Server is started on localhost:3000/')
-});
-//Получение параметров через коллбэк
-application.get('/', (req, res) => {
-    res.end('Test message');
+    //Лог для консоли
+    console.log('Server is started on localhost:3000/');
 });
